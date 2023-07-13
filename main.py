f76c67dba8cd40c86b4dec59864eaf4e15cf8801
Furkan Liman <limanfurkan@outlook.com>
2023-07-13 15:50:52 +0300

    Double Pendelum added. Efficiency increased.

--- main.py
+++ main.py
@@ -13,31 +13,57 @@ class Body:
     timeStep= 0.05
     def __init__(self,*args):
         # free fall
-        if len(args)==6:
-            mass, radius, coorX, coorY, vb,vbAngle = args
+        if len(args)==5:
+            mass, coorX, coorY, vb,vbAngle = args
             vbAngle=-vbAngle
             self.mass= mass
-            self.volume= (4/3)*(math.pi*math.pow(radius,3))
             self.coorX = coorX
             self.coorY = coorY
             self.vb=vb
             self.vbX,self.vbY= self.vbAngleCalculate(vb,vbAngle)
-            self.freeFallAnimation()
-            
+            self.vx, self.vy= 0,0
+            self.freeFallAnimation()    
         # pendelum
-        elif len(args)==7:
-            mass, radius, coorX, coorY, Qmax, Len,tailLen = args
+        elif len(args)==6:
+            mass, coorX, coorY, Qmax, Len,tailLen = args
             self.Qmax= Qmax
+            self.Q=Qmax
             self.T = physic.findPeriodPendelum(Len)
             self.Len=Len
             self.tailLen=tailLen
             self.mass= mass
-            self.volume= (4/3)*(math.pi*math.pow(radius,3))
             self.coorX = coorX
             self.coorY = coorY
             self.pendelumAnimation()
-    
-    def cleanDrawFreeFall(space,pathXY):
+        # double pendelum
+        elif len(args)==9:
+            m1,m2,L1,L2,Q1,Q2,Q1V,Q2V,tailLen = args
+            
+            Q1 = Q1 * math.pi / 180
+            Q2 = Q2 * math.pi / 180
+            
+            x1 = L1 * math.sin(Q1)
+            y1 = L1 * math.cos(Q1)
+
+            x2 = x1 + L2 * math.sin(Q2)
+            y2 = y1 + L2 * math.cos(Q2)
+            
+            self.m1=m1
+            self.m2=m2
+            self.L1=L1
+            self.L2=L2
+            self.Q1=Q1
+            self.Q2=Q2
+            self.Q1V=Q1V
+            self.Q2V=Q2V
+            self.x1=x1
+            self.y1=y1
+            self.x2=x2
+            self.y2=y2
+            self.tailLen=tailLen
+            self.doublePendelumAnimation()
+            
+    def cleanDrawFreeFall(self,space,pathXY,t):
         def drawPathFreeFall(space,pathXY):
             for i in pathXY:
                 x,y=i
@@ -48,12 +74,22 @@ class Body:
         
         space[:,:]=0
         
+        V=int(math.sqrt((self.vx**2)+(self.vy**2)))
+        xint,yint,Vxint,Vyint = int(self.coorX),int(self.coorY),int(self.vx),int(self.vy)
         for i in range(space.shape[1]//100):
             space[(2*i)*100:(2*i+1)*100,int(space.shape[0]*0.98):space.shape[0]] = 255
             space[(2*i+1)*100:(2*i+3)*100,int(space.shape[0]*0.98):space.shape[0]] = 125
             space[int(space.shape[0]*0.98):space.shape[0],(2*i)*100:(2*i+1)*100] = 255
             space[int(space.shape[0]*0.98):space.shape[0],(2*i+1)*100:(2*i+3)*100] = 125
         cv2.putText(space,"100m",(20,int(space.shape[0]-5)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0),2)
+        cv2.putText(space,f"y={self.coorY:.3f} meter",(15,15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+        cv2.putText(space,f"V={V:.3f} m/s",(15,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+        cv2.putText(space,f"t={t:.3f} s",(15,45),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+        cv2.arrowedLine(space,(xint,yint),(xint+Vxint,yint+Vyint),(255,0,0),3) # total-> blue
+        cv2.arrowedLine(space,(xint,yint),(xint,yint+Vyint),(0,255,0),3) # y arrow -> green
+        cv2.arrowedLine(space,(xint,yint),(xint+Vxint,yint),(0,0,255),3) # x arrow -> red
+        cv2.circle(space,(xint,yint),5,(0,0,255),self.mass)
+        
         drawPathFreeFall(space,pathXY)  
     
     def vbAngleCalculate(self,vb,vbAngle):
@@ -63,46 +99,22 @@ class Body:
         return vbX,vbY 
     
     def freeFallAnimation(self):    
-        v=0
-        vx=0
-        vy=0
+    
         t= Body.t
-        x=self.coorX
-        y=self.coorY
         timeStep= Body.timeStep
-        pathXY=physic.drawRoute(self,t,timeStep,space)
+        pathXY=physic.drawRouteFreeFall(self,t,space)
+        
         while True:
             start = time.time()
             
             cv2.imshow("space",space)
-
+            
             if self.coorY>=space.shape[0]*0.98-r:
                 v=0
                 break
 
-            Body.cleanDrawFreeFall(space,pathXY)
-
-            x,y,vy,vx=physic.XYdisplacementFreeFall(self,t,timeStep)
-            
-            #tam sayıları kaldır
-            xint=int(x)
-            yint=int(y)
-            Vxint=int(vx)
-            Vyint=int(vy)
-            vint=int(math.sqrt((vx**2)+(vy**2)))
-            
-
-            self.coorX=x
-            self.coorY=y
-
-            cv2.putText(space,f"y={yint} meter",(15,15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
-            cv2.putText(space,f"V={vint} m/s",(15,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
-            cv2.putText(space,f"t={t} s",(15,45),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
-            cv2.arrowedLine(space,(xint,yint),(xint+Vxint,yint+Vyint),(255,0,0),3) # total-> blue
-            cv2.arrowedLine(space,(xint,yint),(xint,yint+Vyint),(0,255,0),3) # y arrow -> green
-            cv2.arrowedLine(space,(xint,yint),(xint+Vxint,yint),(0,0,255),3) # x arrow -> red
-            cv2.circle(space,(xint,yint),5,(0,0,255),r)
-            
+            self =physic.XYdisplacementFreeFall(self,t)
+            self.cleanDrawFreeFall(space,pathXY,t)       
             
             t+=timeStep
             end = time.time()
@@ -125,7 +137,7 @@ class Body:
         cv2.waitKey(1)
         cv2.destroyAllWindows()
           
-    def cleanDrawPendelum(self,originP,xyL,Q,t):
+    def cleanDrawPendelum(self,originP,xyL,t):
         def drawPathPendelum(space,xyL):
             for i in xyL:
                 x,y=i
@@ -133,9 +145,16 @@ class Body:
         
         x=self.coorX
         y=self.coorY
+        x=space.shape[0]//2+x
         T=self.T
         L=self.Len
-               
+        Q = self.Q
+        
+        xyL.append((x,y))
+        if len(xyL)>self.tailLen:
+            xyL.remove(xyL[0])
+            
+        
         Qdegree = Q*(180/math.pi)
         a = g*math.tan(Q)
         x=int(x)
@@ -148,6 +167,7 @@ class Body:
             space[int(space.shape[0]*0.98):space.shape[0],(2*i+1)*100:(2*i+3)*100] = 125
         cv2.line(space,originP,(originP[0],(originP[1]+1)*L),(125,125,125),1)
         cv2.line(space,originP,(x,y),(255,255,255),3)
+        cv2.circle(space,(x,y),5,(0,0,255),self.mass)
         cv2.putText(space,"100m",(20,int(space.shape[0]-5)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0),2)
         cv2.putText(space,f"T(period)=  {T:.3f}",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
         cv2.putText(space,f"Q(Angle) =  {Qdegree:.3f}",(20,40),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
@@ -160,8 +180,7 @@ class Body:
         Qmaks=self.Qmax
         t=Body.t
         timeStep= Body.timeStep
-        xS=space.shape[0]//2
-        x,y=0,0
+       
         xyL=[]
         
         originP=(space.shape[0]//2,0)
@@ -172,18 +191,9 @@ class Body:
             
             cv2.imshow("space",space)
 
-
-            Q,x,y=physic.XYdisplacementPendelum(self.T,Qmaks,t,self.Len)
-            x=xS+x
-            self.coorX=x
-            self.coorY=y
-            
-            xyL.append((x,y))
-            if len(xyL)>tailLen:
-                xyL.remove(xyL[0])
-            
-            Body.cleanDrawPendelum(self,originP,xyL,Q,t)
-            
+            self.T=physic.findPeriodPendelum(self.Len)
+            self=physic.XYdisplacementPendelum(self,Qmaks,t,self.Len)
+            self.cleanDrawPendelum(originP,xyL,t)
             
             t+=timeStep
             end = time.time()
@@ -206,6 +216,102 @@ class Body:
         cv2.waitKey(1)
         cv2.destroyAllWindows()
 
+    def cleanDrawDoublePendelum(self,space,xyL):
+        def drawPathPendelum(space,xyL):
+            for i in xyL:
+                x,y=i
+                space[int(y),int(x)]=255    
+    
+        space[:,:]=0
+
+        drawPathPendelum(space,xyL)
+        
+        for i in range(space.shape[1]//100):
+            space[(2*i)*100:(2*i+1)*100,int(space.shape[0]*0.98):space.shape[0]] = 255
+            space[(2*i+1)*100:(2*i+3)*100,int(space.shape[0]*0.98):space.shape[0]] = 125
+            space[int(space.shape[0]*0.98):space.shape[0],(2*i)*100:(2*i+1)*100] = 255
+            space[int(space.shape[0]*0.98):space.shape[0],(2*i+1)*100:(2*i+3)*100] = 125
+        
+        midX = space.shape[0]//2
+        midY = space.shape[1]//2
+        
+        x1 = self.x1
+        y1 = self.y1
+        x2 = self.x2
+        y2 = self.y2
+        
+        x1,y1,x2,y2=int(x1)+midX,int(y1)+midY,int(x2)+midX,int(y2)+midY
+        
+        Q1degree= self.Q1*(180/math.pi)
+        Q2degree= self.Q2*(180/math.pi)
+        
+        cv2.line(space,(500,500),(x1,y1),(200,200,255),1)
+        cv2.line(space,(x1,y1),(x2,y2),(200,255,200),2)  
+        cv2.circle(space,(x1,y1),5,(255,0,0),self.m1)
+        cv2.circle(space,(x2,y2),5,(0,0,255),self.m2)
+        cv2.putText(space,"100m",(20,int(space.shape[0]-5)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0),2)
+        cv2.putText(space,f"Q1=  {Q1degree:.3f}",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.putText(space,f"Q2=  {Q2degree:.3f}",(20,40),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.putText(space,f"Q1V=  {self.Q1V:.3f}",(20,60),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.putText(space,f"Q2V=  {self.Q2V:.3f}",(20,80),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+                 
+    def doublePendelumAnimation(self):
+        t=Body.t
+        tailLen=self.tailLen
+        
+        m1=self.m1
+        m2=self.m2
+        L1=self.L1
+        L2=self.L2
+        Q1=self.Q1
+        Q2=self.Q2
+        Q1V=self.Q1V
+        Q2V=self.Q2V
+        
+        timeStep=self.timeStep
+
+        Q1dd,Q2dd=0,0
+        
+        xyL=[]
+
+        while True:
+            start = time.time()
+            
+            cv2.imshow("space",space)
+            
+            self=physic.XYdisplacementDoublePendelum(self)
+
+            self.cleanDrawDoublePendelum(space,xyL)
+            
+            xyL.append((self.x2+space.shape[0]//2,self.y2+space.shape[1]//2))
+            
+            if len(xyL)>=tailLen:
+                xyL.remove(xyL[0])
+            
+            
+            t+=timeStep
+            #time.sleep(timeStep)#sil bunu
+            end = time.time()
+            spentTime= end-start # spent time in while loop
+            """
+            if timeStep-spentTime<=0:
+                time.sleep(0)
+            else:
+                time.sleep(timeStep-spentTime)   
+            """
+            if cv2.waitKey(1) == ord('q'):
+                break
+            
+
+        while True:
+            cv2.imshow("space",space)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
+        
+    
 #------ Common Things -------
 # m = mass, r = radius, (coorX,locaitonY) = instant coors
 #------ Free Fall -----------
@@ -214,21 +320,30 @@ class Body:
 # Qmax=starting degree for pendelum, T = period L = rope lenght 
 
 #---Common variables---
-m,r=1,10
+m,r=5,10
 #---Pendelum variables---
-Qmax, Len, tailLen = 30, 500, 200
+Qmax, Len, tailLen = 30, 200, 200
+coorX,coorY=300,800
+#---double Pen-------
+m1,m2=2,2
+L1,L2=200,200
+Q1,Q2=90,90
+Q1V,Q2V=0,0
 #---Free fall variables---
-vb,vbAngle=50,45
-coorX,coorY=00,500
+vb,vbAngle=80,45
+
 
 while True: 
-    #choice = "2"
-    choice = input("FreeFall (1) or Pendelum (2)= ")
+    choice = "3"
+    #choice = input("FreeFall (1) or Pendelum (2) or Double Pendelum(3)= ")
     if choice == "1":
-        b1 = Body(m, r, coorX,coorY,vb,vbAngle)
+        b1 = Body(m, coorX,coorY,vb,vbAngle)
         break
     elif choice=="2":
-        b1 = Body(m,r,coorX,coorY,Qmax,Len,tailLen)
+        b1 = Body(m, coorX,coorY,Qmax,Len,tailLen)
+        break
+    elif choice=="3":
+        b1 = Body(m1,m2,L1,L2,Q1,Q2,Q1V,Q2V,tailLen)
         break
     else:
         continue
--- physic.py
+++ physic.py
@@ -14,19 +14,29 @@ def findPeriodPendelum(L):
     T=2*math.pi*math.sqrt((L/g))
     return T
 
-def XYdisplacementPendelum(T,Qmaks,t,L):
+def XYdisplacementPendelum(body,Qmaks,t,L):
+    T = body.T
     Q = Qmaks*math.cos((((2*math.pi)/T)*t))
-    print(f"{Q}         {t}     {Qmaks}")
+    
     x=L*math.sin(Q)
     y=L*math.cos(Q)
-    return Q,x,y
+    
+    body.Q = Q
+    body.coorX=x
+    body.coorY=y
+    return body
 
-def XYdisplacementFreeFall(body,t,timeStep):
+def XYdisplacementFreeFall(body,t):
+    timeStep= body.timeStep
     x=body.coorX
     y=body.coorY
     m=body.mass
     vbX=body.vbX
     vbY=body.vbY
+    vx=body.vx
+    vy=body.vy
+    
+    vx=vbX
     x+=timeStep*(vbX)
     f = m * g
     a = f/m
@@ -34,8 +44,18 @@ def XYdisplacementFreeFall(body,t,timeStep):
     vy2 = vy
     vy1 = vbY+a*(t-timeStep)
     y+=timeStep*((vy1+vy2)/2)
-    return x,y,vy,vbX
-def drawRoute(body,t,timeStep,space):
+    
+    body.coorX = x
+    body.coorY = y
+
+    body.vx = vx
+    body.vy = vy
+    
+
+    return body
+
+def drawRouteFreeFall(body,t,space):
+    timeStep= body.timeStep
     pathXY=[]
     x=body.coorX
     y=body.coorY
@@ -55,3 +75,42 @@ def drawRoute(body,t,timeStep,space):
         if y>=space.shape[0]*0.98:
             break
     return pathXY 
+
+def XYdisplacementDoublePendelum(body):
+
+    m1=body.m1
+    m2=body.m2
+    L1=body.L1
+    L2=body.L2
+    Q1=body.Q1
+    Q2=body.Q2
+    Q1V=body.Q1V
+    Q2V=body.Q2V
+    timestep = body.timeStep
+
+    Q1a = ((-g * (2 * m1 + m2) * math.sin(Q1))+(-m2 * g * math.sin(Q1-2*Q2)) + (-2*math.sin(Q1-Q2)*m2)*(Q2V*Q2V*L2+Q1V*Q1V*L1*math.cos(Q1-Q2))) / (L1 * (2*m1+m2-m2*math.cos(2*Q1-2*Q2)))
+
+    Q2a = ((2 * math.sin(Q1-Q2))*(((Q1V*Q1V*L1*(m1+m2)))+(g * (m1 + m2) * math.cos(Q1))+(Q2V*Q2V*L2*m2*math.cos(Q1-Q2)))) / (L2 * (2*m1+m2-m2*math.cos(2*Q1-2*Q2)))
+
+    x1 = L1 * math.sin(Q1)
+    y1 = L1 * math.cos(Q1)
+
+    x2 = x1 + L2 * math.sin(Q2)
+    y2 = y1 + L2 * math.cos(Q2)
+
+    Q1V += Q1a*timestep
+    Q2V += Q2a*timestep
+    Q1 += Q1V*timestep
+    Q2 += Q2V*timestep
+    
+    body.Q1 = Q1
+    body.Q2 = Q2
+    body.Q1V= Q1V
+    body.Q2V= Q2V
+    
+    body.x1 = x1
+    body.y1 = y1
+    body.x2 = x2
+    body.y2 = y2
+    
+    return body
