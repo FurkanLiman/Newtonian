b9e31671b16a078a64f058c7915d8614fa8d7881
Furkan Liman <limanfurkan@outlook.com>
2023-07-14 17:18:15 +0300

    Selecting menu added. Bug fixed.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 import physic
 
 space = np.zeros([1000,1000,3],dtype=np.uint8)
-
+space1 = np.zeros([1000,1000,3],dtype=np.uint8)
 g=physic.g
 
 class Body:
@@ -122,6 +122,11 @@ class Body:
         cv2.createTrackbar("TimeStep","TrackBars",5,100,empty)
         cv2.createTrackbar("RealTime","TrackBars",0,1,empty)
         cv2.createTrackbar("Open_Close","TrackBars",1,1,empty)
+        cv2.setTrackbarMin("TimeStep","TrackBars",1)
+        cv2.setTrackbarMin("mass","TrackBars",1)
+        cv2.setTrackbarMin("g","TrackBars",1)
+        cv2.setTrackbarMin("Len","TrackBars",5)
+        cv2.setTrackbarMin("tailLen","TrackBars",10)
         t=self.t
         timeStep= Body.timeStep
         Close=1
@@ -141,20 +146,10 @@ class Body:
                 Close = cv2.getTrackbarPos("Open_Close","TrackBars")
                 Body.timeStep = cv2.getTrackbarPos("TimeStep","TrackBars")/100
                 realOrStep = cv2.getTrackbarPos("RealTime","TrackBars")
-                
-                if Body.timeStep==0:
-                    Body.timeStep=0.01
-                if self.mass==0:
-                    self.mass=1 
+                 
                 if Close==0:
                     break  
-                if physic.g==0:
-                    physic.g=1
-                if self.Len < 4:
-                    self.Len=5
-                if self.tailLen<10:
-                    self.tailLen=10 
-             
+                
                 self.T=physic.findPeriodPendelum(self.Len)
                 self=physic.XYdisplacementPendelum(self,self.t,self.Len)
                 self.cleanDrawPendelum(originP,xyL,self.t,realOrStep)
@@ -184,27 +179,27 @@ class Body:
         cv2.waitKey(1)
         cv2.destroyAllWindows()
 
-    def cleanDrawDoublePendelum(self,space,xyL,realOrStep):
-        def drawPathPendelum(space,xyL):
+    def cleanDrawDoublePendelum(self,space1,xyL,realOrStep):
+        def drawPathPendelum(space1,xyL):
             a = [0,0,0]
             for i in xyL:
                 a[2]+=255//len(xyL)
                 x,y=i
-                if x > 0 and x < space.shape[0] and y > 0 and  y < space.shape[1]:
-                    space[int(y),int(x)]=a
+                if x > 0 and x < space1.shape[0] and y > 0 and  y < space1.shape[1]:
+                    space1[int(y),int(x)]=a
     
-        space[:,:]=0
+        space1[:,:]=0
 
-        drawPathPendelum(space,xyL)
+        drawPathPendelum(space1,xyL)
         
-        for i in range(space.shape[1]//100):
-            space[(2*i)*100:(2*i+1)*100,int(space.shape[0]*0.98):space.shape[0]] = 255
-            space[(2*i+1)*100:(2*i+3)*100,int(space.shape[0]*0.98):space.shape[0]] = 125
-            space[int(space.shape[0]*0.98):space.shape[0],(2*i)*100:(2*i+1)*100] = 255
-            space[int(space.shape[0]*0.98):space.shape[0],(2*i+1)*100:(2*i+3)*100] = 125
+        for i in range(space1.shape[1]//100):
+            space1[(2*i)*100:(2*i+1)*100,int(space1.shape[0]*0.98):space1.shape[0]] = 255
+            space1[(2*i+1)*100:(2*i+3)*100,int(space1.shape[0]*0.98):space1.shape[0]] = 125
+            space1[int(space1.shape[0]*0.98):space1.shape[0],(2*i)*100:(2*i+1)*100] = 255
+            space1[int(space1.shape[0]*0.98):space1.shape[0],(2*i+1)*100:(2*i+3)*100] = 125
         
-        midX = space.shape[0]//2
-        midY = space.shape[1]//2
+        midX = space1.shape[0]//2
+        midY = space1.shape[1]//2
         
         x1 = self.x1
         y1 = self.y1
@@ -216,7 +211,7 @@ class Body:
         Q1degree= self.Q1*(180/math.pi)
         Q2degree= self.Q2*(180/math.pi)
         
-        xyL.append((self.x2+space.shape[0]//2,self.y2+space.shape[1]//2))
+        xyL.append((self.x2+space1.shape[0]//2,self.y2+space1.shape[1]//2))
         fark=len(xyL)-self.tailLen
         try:
             if fark>0:
@@ -224,25 +219,22 @@ class Body:
                     xyL.remove(xyL[i])
         except:
             pass
-        cv2.line(space,(500,500),(x1,y1),(200,200,255),1)
-        cv2.line(space,(x1,y1),(x2,y2),(200,255,200),2)  
-        cv2.circle(space,(x1,y1),5,(255,0,0),self.m1)
-        cv2.circle(space,(x2,y2),5,(0,0,255),self.m2)
-        cv2.putText(space,"100m",(20,int(space.shape[0]-5)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0),2)
-        cv2.putText(space,f"Q1=  {Q1degree:.3f}",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
-        cv2.putText(space,f"Q2=  {Q2degree:.3f}",(20,40),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
-        cv2.putText(space,f"Q1V=  {self.Q1V:.3f}",(20,60),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
-        cv2.putText(space,f"Q2V=  {self.Q2V:.3f}",(20,80),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        
+        cv2.line(space1,(500,500),(x1,y1),(200,200,255),1)
+        cv2.line(space1,(x1,y1),(x2,y2),(200,255,200),2)  
+        cv2.circle(space1,(x1,y1),5,(255,0,0),self.m1)
+        cv2.circle(space1,(x2,y2),5,(0,0,255),self.m2)
+        cv2.putText(space1,"100m",(20,int(space1.shape[0]-5)),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0),2)
+        cv2.putText(space1,f"Q1=  {Q1degree:.3f}",(20,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.putText(space1,f"Q2=  {Q2degree:.3f}",(20,40),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.putText(space1,f"Q1V=  {self.Q1V:.3f}",(20,60),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.putText(space1,f"Q2V=  {self.Q2V:.3f}",(20,80),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
         if realOrStep==1:
-            cv2.putText(space,"Real Time ON",(20,100),cv2.FONT_HERSHEY_SIMPLEX,0.5,(10,10,225),1)
+            cv2.putText(space1,"Real Time ON",(20,100),cv2.FONT_HERSHEY_SIMPLEX,0.5,(10,10,225),1)
         elif realOrStep==0:
-            cv2.putText(space,"Time Step ON",(20,100),cv2.FONT_HERSHEY_SIMPLEX,0.5,(10,225,10),1)
-        
-                 
-    def doublePendelumAnimation(self):
-        t=Body.t
-        tailLen=self.tailLen
-        
+            cv2.putText(space1,"Time Step ON",(20,100),cv2.FONT_HERSHEY_SIMPLEX,0.5,(10,225,10),1)
+                  
+    def doublePendelumAnimation(self):      
         m1=self.m1
         m2=self.m2
         L1=self.L1
@@ -251,11 +243,6 @@ class Body:
         Q2=self.Q2
         Q1V=self.Q1V
         Q2V=self.Q2V
-        
-        timeStep=self.timeStep
-
-        
-       
 
         def empty(a):
             pass    
@@ -265,63 +252,52 @@ class Body:
         def Qchange2(a):
             Qs = a * math.pi / 180  
             self.Q2=Qs
-        cv2.namedWindow("TrackBars")
-        cv2.resizeWindow("TrackBars",640,500)
-        cv2.createTrackbar("mass1","TrackBars",2,25,empty)
-        cv2.createTrackbar("mass2","TrackBars",2,25,empty)
-        cv2.createTrackbar("Q1","TrackBars",90,360,Qchange1)
-        cv2.createTrackbar("Q2","TrackBars",90,360,Qchange2)
-        cv2.createTrackbar("g","TrackBars",98,300,empty)
-        cv2.createTrackbar("Len1","TrackBars",200,500,empty)
-        cv2.createTrackbar("Len2","TrackBars",200,500,empty)
-        cv2.createTrackbar("tailLen","TrackBars",200,800,empty)
-        cv2.createTrackbar("TimeStep","TrackBars",5,100,empty)
-        cv2.createTrackbar("RealTime","TrackBars",0,1,empty)
-        cv2.createTrackbar("Open_Close","TrackBars",1,1,empty)
+        cv2.namedWindow("TrackBarsDouble")
+        cv2.resizeWindow("TrackBarsDouble",640,500)
+        cv2.createTrackbar("mass1","TrackBarsDouble",2,25,empty)
+        cv2.createTrackbar("mass2","TrackBarsDouble",2,25,empty)
+        cv2.createTrackbar("Q1","TrackBarsDouble",90,360,Qchange1)
+        cv2.createTrackbar("Q2","TrackBarsDouble",90,360,Qchange2)
+        cv2.createTrackbar("g","TrackBarsDouble",98,300,empty)
+        cv2.createTrackbar("Len1","TrackBarsDouble",200,500,empty)
+        cv2.createTrackbar("Len2","TrackBarsDouble",200,500,empty)
+        cv2.createTrackbar("tailLen","TrackBarsDouble",200,800,empty)
+        cv2.createTrackbar("TimeStep","TrackBarsDouble",5,100,empty)
+        cv2.createTrackbar("RealTime","TrackBarsDouble",0,1,empty)
+        cv2.createTrackbar("Stop","TrackBarsDouble",0,1,empty)
+        cv2.setTrackbarMin("TimeStep","TrackBarsDouble",1)
+        cv2.setTrackbarMin("mass1","TrackBarsDouble",1)
+        cv2.setTrackbarMin("mass2","TrackBarsDouble",1)
+        cv2.setTrackbarMin("g","TrackBarsDouble",1)
+        cv2.setTrackbarMin("Len1","TrackBarsDouble",5)
+        cv2.setTrackbarMin("Len2","TrackBarsDouble",5)
+        cv2.setTrackbarMin("tailLen","TrackBarsDouble",10)
         Close=1
         while Close==1:
             
             xyL=[]
             while True:
+                
                 start = time.time()
                 
-                self.m1= cv2.getTrackbarPos("mass1","TrackBars") 
-                self.m2= cv2.getTrackbarPos("mass2","TrackBars")
-                physic.g= cv2.getTrackbarPos("g","TrackBars")/10
-                self.L1 = cv2.getTrackbarPos("Len1","TrackBars")
-                self.L2 = cv2.getTrackbarPos("Len2","TrackBars")   
-                self.tailLen = cv2.getTrackbarPos("tailLen","TrackBars")
-                Close =cv2.getTrackbarPos("Open_Close","TrackBars")  
-                ts = cv2.getTrackbarPos("TimeStep","TrackBars")/100
-                realOrStep = cv2.getTrackbarPos("RealTime","TrackBars")
+                self.m1= cv2.getTrackbarPos("mass1","TrackBarsDouble") 
+                self.m2= cv2.getTrackbarPos("mass2","TrackBarsDouble")
+                physic.g= cv2.getTrackbarPos("g","TrackBarsDouble")/10
+                self.L1 = cv2.getTrackbarPos("Len1","TrackBarsDouble")
+                self.L2 = cv2.getTrackbarPos("Len2","TrackBarsDouble")   
+                self.tailLen = cv2.getTrackbarPos("tailLen","TrackBarsDouble")
+                Close =cv2.getTrackbarPos("Stop","TrackBarsDouble")  
+                ts = cv2.getTrackbarPos("TimeStep","TrackBarsDouble")/100
+                realOrStep = cv2.getTrackbarPos("RealTime","TrackBarsDouble")
                 
-                if ts==0:
-                    ts=0.01
-                if self.m1==0:
-                    self.m1=1 
-                if self.m2==0:
-                    self.m2=1
                 if Close==0:
                     break  
-                if physic.g==0:
-                    physic.g=0.1
-                if self.L1 < 4:
-                    self.L1=5
-                if self.L2 < 4:
-                    self.L2=5
-                if self.tailLen<10:
-                    self.tailLen=10 
-                
-                
+              
                 self=physic.XYdisplacementDoublePendelum(self)
 
-                self.cleanDrawDoublePendelum(space,xyL,realOrStep)
+                self.cleanDrawDoublePendelum(space1,xyL,realOrStep)
                 
-                
-                cv2.imshow("space",space)
-                
-                
-                
+                cv2.imshow("space1",space1)
                 
                 end = time.time()
                 if realOrStep==1:
@@ -341,14 +317,13 @@ class Body:
                 
 
             while True:
-                cv2.imshow("space",space)
+                cv2.imshow("space1",space1)
                 if cv2.waitKey(1) == ord('q'):
                     break
 
         cv2.waitKey(1)
         cv2.destroyAllWindows()
         
-    
 #------ Common Things -------
 # m = mass, (coorX,locaitonY) = instant coors
 #-----  Pendelum ------------
@@ -366,15 +341,29 @@ Q1,Q2=90,90
 Q1V,Q2V=0,0
 
 
-while True: 
-    choice = "2"
-    #choice = input("FreeFall (1) or Pendelum (2) or Double Pendelum(3)= ")
+def empty(a):
+    pass    
 
-    if choice=="1":
-        b1 = Body(m, coorX,coorY,Qmax,Len,tailLen)
+cv2.namedWindow("Select")
+cv2.resizeWindow("Select",350,90)
+cv2.createTrackbar("MonoDouble","Select",0,1,empty)
+cv2.createTrackbar("Start","Select",0,1,empty)
+
+while True: 
+    
+    start = cv2.getTrackbarPos("Start","Select")
+    monodouble= cv2.getTrackbarPos("MonoDouble","Select")
+
+    if start==1:
+        cv2.destroyWindow("Select")
+        if  monodouble == 0:             
+            b1 = Body(m, coorX,coorY,Qmax,Len,tailLen)
+        elif monodouble==1:
+            b2 = Body(m1,m2,L1,L2,Q1,Q2,Q1V,Q2V,tailLen)
         break
-    elif choice=="2":
-        b1 = Body(m1,m2,L1,L2,Q1,Q2,Q1V,Q2V,tailLen)
+    
+    if cv2.waitKey(1) == ord('q'):
         break
-    else:
-        continue
+
+cv2.waitKey(1)
+cv2.destroyAllWindows()
